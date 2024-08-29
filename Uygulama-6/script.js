35e4c2bcec2157adc9880dd74b9e5501430b86bf
Eakn54 <akin1.emre@gmail.com>
2024-08-29 15:17:17 +0300

    Add files via upload

--- Uygulama-6/script.js
+++ Uygulama-6/script.js
@@ -11,18 +11,22 @@ function updateCounter() {
     }
 }
 
-const maxİmage = 3;
-let i = 0;
-let imageDom = document.getElementsByClassName("slideİmage")
+let index = 0;
+let imageDom = document.getElementsByClassName("slideImage");
+let slideTimer;
 
-
-document.addEventListener("DOMContentLoaded", imageChanger);
+document.addEventListener("DOMContentLoaded", function () {
+    imageChanger();
+    slideTimer = setInterval(imageChanger, 3000);
+});
 
 function imageChanger() {
-    imageDom[i].style.display = "block";
-    i++;
-}
-if (i < maxİmage) {
-    setTimeout(imageChanger, 10000);
-
+    for (i = 0; i < imageDom.length; i++) {
+        imageDom[i].style.display = "none";
+    }
+    index++;
+    if (index > imageDom.length) {
+        index = 1;
+    }
+    imageDom[index - 1].style.display = "block";
 }
